fafe60b074137b6686736c8e7008f9ecab528eac
nicordev <nicolas.renvoise.dev@gmail.com>
2019-09-24 11:59:16 +0200

    Read the role hierarchy

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -2,17 +2,28 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Helper\RoleHierarchyReader;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Security\Core\Role\RoleHierarchyInterface;
 
 class UserType extends AbstractType
 {
+    /**
+     * @var array
+     */
+    private $roles = [];
+
+    public function __construct(RoleHierarchyInterface $roleHierarchy)
+    {
+        $this->roles = RoleHierarchyReader::read($roleHierarchy);
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -27,10 +38,7 @@ class UserType extends AbstractType
             ->add('email', EmailType::class, ['label' => 'Adresse email'])
             ->add('roles', ChoiceType::class, [
                 'multiple' => true,
-                'choices' => [
-                    "Utilisateur" => "ROLE_USER",
-                    "Administrateur" => "ROLE_ADMIN"
-                ]
+                'choices' => $this->roles
             ])
         ;
     }
